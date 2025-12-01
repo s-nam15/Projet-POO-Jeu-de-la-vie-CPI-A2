c8de46d29f2951df5d4216a12231c7a3edf90d6b
root <root@FRUH124476.localdomain>
2025-12-01 23:55:28 +0100

    Implementation du main

--- src/main.cpp
+++ src/main.cpp
@@ -5,3 +5,15 @@
 #include "ConsoleRenderer.h"
 #include "Game.h"
 
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <input-file> [iterations]\n";
+        return 1;
+    }
+    std::string path = argv[1];
+    int iterations = 10;
+    if (argc >= 3) iterations = std::stoi(argv[2]);
+
+
+    return 0;
+}
